0ec219c434d95e29db5876150ce20ce365905b30
rocky chen <rocky.cn@foxmail.com>
2018-09-27 14:40:53 +0800

    Update centos7-docker-install.sh

--- centos7-docker-install.sh
+++ centos7-docker-install.sh
@@ -122,7 +122,43 @@ function setupDocker()
 	sudo yum install -y yum-utils \
         device-mapper-persistent-data \
         lvm2
+	
+    sudo tee /etc/security/limits.conf <<-'EOF'
+#增加如下内容
+root soft nofile 102400
+root hard nofile 102400
+* soft nofile 102400
+* hard nofile 102400
+EOF
 
+    sudo tee /etc/sysctl.conf <<-'EOF'
+#增加如下内容
+net.ipv4.neigh.default.gc_stale_time=120
+net.ipv4.conf.all.rp_filter=0
+net.ipv4.conf.default.rp_filter=0
+net.ipv4.conf.default.arp_announce=2
+net.ipv4.conf.all.arp_announce=2
+net.ipv4.tcp_max_tw_buckets=5000
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_max_syn_backlog=1024
+net.ipv4.tcp_synack_retries=2
+#net.ipv6.conf.all.disable_ipv6=1
+#net.ipv6.conf.default.disable_ipv6=1
+#net.ipv6.conf.lo.disable_ipv6=1
+net.ipv4.conf.lo.arp_announce=2
+vm.swappiness=10
+vm.vfs_cache_pressure=50
+vm.overcommit_memory=1
+net.core.somaxconn=65535
+net.netfilter.nf_conntrack_max=655350
+net.netfilter.nf_conntrack_tcp_timeout_established=1200
+#增加NFS挂载服务并发数
+sunrpc.tcp_slot_table_entries=128
+EOF
+
+    #使修改马上生效
+    sudo sysctl -p
+    
     # 安装Docker
     curl https://releases.rancher.com/install-docker/17.03.sh | sh
     
